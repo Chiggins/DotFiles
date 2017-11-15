97106b5922c7384db6a34b077ae33b61c2763ccf
Chris Higgins <chris@chigs.me>
2017-11-15 01:11:50 -0600

    More logic to the polybar launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,5 +4,14 @@ killall -q polyball
 
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar tophdmi &
-polybar toplaptop &
+if polybar -m | grep -i "Virtual"; then
+    polybar topsinglevm
+fi
+
+if polybar -m | grep -i "eDP"; then
+    polybar toplaptop
+fi
+
+if polybar -m | grep -i "HDMI"; then
+    polybar tophdmi
+fi
