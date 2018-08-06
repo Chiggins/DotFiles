c4f6e367f7019109215187e950a8270f6739be06
Chris Higgins <chris@chigs.me>
2018-08-05 22:50:00 -0500

    Quiet and fixed tmux download

--- refresh.sh
+++ refresh.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # ZSH
-wget https://github.com/Chiggins/Dotfiles/raw/master/general/zshrc -O ~/.zshrc
-wget https://github.com/Chiggins/DotFiles/raw/master/general/chiggins.zsh-theme -O ~/.oh-my-zsh/themes/chiggins.zsh-theme
+wget -q https://github.com/Chiggins/Dotfiles/raw/master/general/zshrc -O ~/.zshrc
+wget -q https://github.com/Chiggins/DotFiles/raw/master/general/chiggins.zsh-theme -O ~/.oh-my-zsh/themes/chiggins.zsh-theme
 
 # VIM
 wget -q https://github.com/Chiggins/DotFiles/raw/master/general/vimrc -O ~/.vimrc
 vim +PluginInstall +qall
 
 # TMUX
-wget -q https://raw.githubusercontent.com/Chiggins/DotFiles/master/general/.tmux.conf -O ~/.tmux.conf
+wget -q https://raw.githubusercontent.com/Chiggins/DotFiles/master/general/tmux.conf -O ~/.tmux.conf
 
 # Other customizations
-wget https://github.com/Chiggins/Dotfiles/raw/master/general/gdbinit -O ~/.gdbinit
-wget https://github.com/Chiggins/Dotfiles/raw/master/general/gitconfig -O ~/.gitconfig
-wget https://github.com/Chiggins/Dotfiles/raw/master/general/pythonrc -O ~/.pythonrc
+wget -q https://github.com/Chiggins/Dotfiles/raw/master/general/gdbinit -O ~/.gdbinit
+wget -q https://github.com/Chiggins/Dotfiles/raw/master/general/gitconfig -O ~/.gitconfig
+wget -q https://github.com/Chiggins/Dotfiles/raw/master/general/pythonrc -O ~/.pythonrc
